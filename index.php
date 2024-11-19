855752c75298c82cb363ddf006fac8c89437847e
kronen9999 <stacica22eneronkio@gmail.com>
2024-11-19 12:42:24 -0600

    Se agregaron etiquetas de titulo,formato de texto, viewport y referencia a hoja de estilo al head

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-      
+      <title>Menu principal</title>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="stylesheet" href="Views/Css/Index/index.css">
     </head>
     <body>
         

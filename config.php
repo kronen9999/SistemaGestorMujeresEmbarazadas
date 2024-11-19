ade12fdf551ec97ea260509ec2b4a8fac836f857
kronen9999 <stacica22eneronkio@gmail.com>
2024-11-19 08:00:17 -0600

    Se agreggo el codigo de conexion al archivo config.php

--- config.php
+++ config.php
@@ -1,6 +1,16 @@
 <?php
+$host="localhost";
+$user="root";
+$password="";
+$db="GESTOREMBARAZADAS";
 
+$conexiondb=mysqli_connect($host,$user,$password,$db);
 
-
-
+if ($conexiondb)
+{
+echo "Se ha conectado a la base de datos";
+}
+else {
+    echo "No se ha podido conectar a la base de datos";
+}
 ?>

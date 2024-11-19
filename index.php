bf06b1f25a7773045b0430afdac26b849b321d42
kronen9999 <stacica22eneronkio@gmail.com>
2024-11-19 13:27:08 -0600

    Se agrego el cuerpo de inicio de sesion

--- index.php
+++ index.php
@@ -7,6 +7,16 @@
       <link rel="stylesheet" href="Public/Css/Index/index.css">
     </head>
     <body>
-        
+       <img src="Public/Assets/InicioSesion_Doctores_Fondo.png" alt="ImgDoctor" class="img_sesion"> 
+       <div>
+        <h1>Inicio de sesion</h1>
+        <img src="Public/Assets/InicioSesion_Doctores_IconoDoctor.png" alt="IconoDoctor">
+       <p>Usuario:</p>
+       <input type="text" maxlength="100" placeholder="Ingrese su cedula o correo electronico" class="input_sesion">
+       <p>Contraseña:</p>
+       <input type="password"  class="input_session" placeholder="Ingrese su contraseña">
+       <p>¿Olvidaste tu contraseña?<p id ="p_recuperarContraseña"> De click aqui para recuperarla</p></p>
+       <button type="button">Iniciar sesion</button>
+       </div>
     </body>
 </html>

2375246f55cfa92ce3dda761fe9c06f54048baac
kronen9999 <stacica22eneronkio@gmail.com>
2024-11-22 09:10:47 -0600

    Se corrijio la expresion para el inicio de sesion

--- Models/Index/ValidacionLogin.php
+++ Models/Index/ValidacionLogin.php
@@ -5,11 +5,11 @@
     public function ConsultaDb($tipoLogin, $usuarioop1, $usuarioop2, $contraseña,$consultadb) {
         
         if ($tipoLogin == "Administrador") {
-            $consultaString = "SELECT * FROM ADMINISTRADORES WHERE NoTrabajador = ? OR CorreoElectronico = ? AND Contraseña = ?";
+            $consultaString = "SELECT * FROM ADMINISTRADORES WHERE (NoTrabajador = ? OR CorreoElectronico = ?) AND Contraseña = ?";
         } else if ($tipoLogin == "Doctor") {
-            $consultaString = "SELECT * FROM DOCTORES WHERE Cedula = ? OR CorreoElectronico = ? AND Contraseña = ?";
+            $consultaString = "SELECT * FROM DOCTORES WHERE (Cedula = ? OR CorreoElectronico = ?) AND Contraseña = ?";
         } else if ($tipoLogin == "Paciente") {
-            $consultaString = "SELECT * FROM PACIENTES WHERE CurpPaciente = ? OR CorreoElectronico = ? AND Contraseña = ?";
+            $consultaString = "SELECT * FROM PACIENTES WHERE (CurpPaciente = ? OR CorreoElectronico = ?) AND Contraseña = ?";
         }
 
        

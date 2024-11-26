dae02b2960fcfd5c0fd87dd377e2d729733217ab
kronen9999 <stacica22eneronkio@gmail.com>
2024-11-25 21:36:02 -0600

    Se han agregado botones al menu, se les ha dado  estilo y se ha agregado las imagenes correspondientes a cada boton

--- Views/PanelAdministrador.php
+++ Views/PanelAdministrador.php
@@ -12,7 +12,18 @@
             <img src="../Public/Assets/Administrador_InformacionPerfil_Icono.png" alt="Icono_Administrador">
         </header>
         <div class="menuLateral">
-
+        <div class="botonMenu" botonSeleccionado="true" opcionMenu="botonPerfil">
+        <img src="../Public/Assets/Icono_Administrador_Perfil.png" alt="IconoApartado">
+        </div>
+        <div class="botonMenu" botonSeleccionado="false" opcionMenu="botonClinicas">
+        <img src="../Public/Assets/Icono_Administrador_Clinica.png" alt="IconoApartado">
+        </div>
+        <div class="botonMenu" botonSeleccionado="false" opcionMenu="botonDoctores">
+        <img src="../Public/Assets/Icono_Administraador_Doctor.png" alt="IconoApartado">
+        </div>
+        <div class="botonMenu" botonSeleccionado="false" opcionMenu="botonPacientes">
+        <img src="../Public/Assets/Icono_Administrador_Paciente.png" alt="IconoApartado">
+        </div>
         </div>
 
         <div class="Menu_Dinamico" TipoMenu="Informacion_Administrador">

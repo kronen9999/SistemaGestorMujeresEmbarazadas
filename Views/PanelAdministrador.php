10898f6c770234166ddec2ed6a855ab984b3c3cb
kronen9999 <stacica22eneronkio@gmail.com>
2024-11-27 10:46:35 -0600

    Se agrego el diseño de la pagina principal de administrador

--- Views/PanelAdministrador.php
+++ Views/PanelAdministrador.php
@@ -25,10 +25,90 @@
         <img src="../Public/Assets/Icono_Administrador_Paciente.png" alt="IconoApartado">
         </div>
         </div>
-
         <div class="Menu_Dinamico" TipoMenu="Informacion_Administrador">
+        
+            <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1">
+                <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_DivImg">
+                   <img src="../Public/Assets/Icono_Administrador_Perfil.png" alt="IconoAdmin"> 
+                </div>
+                <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1">
+                    <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput">
+                      <p>
+                         Nombre:
+                      </p>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div">
+                      <input type="text"  class="input_Nombre" placeholder="Ingrese su nombre" id="Perfil_Nombre">  
+                      <img src="../Public/Assets/Icono_Editar.png" alt="IconoEditarImputs">
+                      </div> 
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput">
+                      <p>
+                         Apellido paterno:
+                      </p>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div">
+                      <input type="text"  class="input_Nombre" placeholder="Ingrese su apellido paterno" id="Perfil_ApellidoPaterno">  
+                      <img src="../Public/Assets/Icono_Editar.png" alt="IconoEditarImputs">
+                      </div>
+                    </div>
+                    <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput">
+                      <p>
+                         Apellido Materno:
+                      </p>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div">
+                      <input type="text"  class="input_Nombre" placeholder="Ingrese su apellido materno" id="Perfil_ApellidoMaterno">  
+                      <img src="../Public/Assets/Icono_Editar.png" alt="IconoEditarImputs">
+                      </div>
+                </div>
+            </div>
+        
+        </div>
+        </div>
+        <div class="Menu_Dinamico_AdministrarPerfil_SubApartado2">
+            <div class="Menu_Dinamico_AdministrarPerfil_SubApartado2_Div1">
+            <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput">
+                      <p>
+                         Numero trabajador:
+                      </p>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div">
+                      <input type="text"  class="input_Nombre" placeholder="Ingrese su numero de trabajador" id="Perfil_Notrabajador">  
+                      <img src="../Public/Assets/Icono_Editar.png" alt="IconoEditarImputs">
+                      </div> 
+                      </div>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput">
+                      <p>
+                         Telefono:
+                      </p>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div">
+                      <input type="text"  class="input_Nombre" placeholder="Ingrese su Telefono" id="Perfil_Telefono">  
+                      <img src="../Public/Assets/Icono_Editar.png" alt="IconoEditarImputs">
+                      </div> 
+                      </div>
+            </div>
+            <div class="Menu_Dinamico_AdministrarPerfil_SubApartado2_Div2">
+            <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput">
+                      <p>
+                         Contraseña:
+                      </p>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div">
+                      <input type="text"  class="input_Nombre" placeholder="Ingrese su contraseña" id="Perfil_Contraseña">  
+                      <img src="../Public/Assets/Icono_Editar.png" alt="IconoEditarImputs">
+                      </div> 
+                      </div>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput">
+                      <p>
+                         Correo Electronico:
+                      </p>
+                      <div class="Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div">
+                      <input type="text"  class="input_Nombre" placeholder="Ingrese su Correo electronico" id="Perfil_Correo_Electronico">  
+                      <img src="../Public/Assets/Icono_Editar.png" alt="IconoEditarImputs">
+                      </div> 
+                      </div>
+            </div>
+        </div>
+        </div>
+        
 
         </div>
+
     </body>
 
 </html>

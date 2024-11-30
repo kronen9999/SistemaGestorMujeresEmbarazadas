4c8dc2054d8274253a18c7fa58c30d2a29637b2d
kronen9999 <stacica22eneronkio@gmail.com>
2024-11-30 14:34:08 -0600

    Se agregaron las variables al php que se recibiran del js

--- Controllers/Ajax/PanelAdministrador/InfoAdmin/PerfilAdministrador.php
+++ Controllers/Ajax/PanelAdministrador/InfoAdmin/PerfilAdministrador.php
@@ -1,7 +1,14 @@
 <?php
+include ("../../../../config.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
+$nombre=isset($_POST["nombre"])?$_POST["nombre"]:null;    
+$apellidoP=isset($_POST["nombre"])?$_POST["apellidoP"]:null;    
+$apellidoM=isset($_POST["nombre"])?$_POST["apellidoM"]:null;    
+$noTrabajador=isset($_POST["nombre"])?$_POST["noTrabajador"]:null;    
+$telefono=isset($_POST["nombre"])?$_POST["telefono"]:null;    
+$correoE=isset($_POST["nombre"])?$_POST["correo"]:null;    
+
     
     
 }

51cd4e358ebe647c24f2fb9fdfb66d300124f6a3
kronen9999 <stacica22eneronkio@gmail.com>
2024-12-01 20:24:46 -0600

    Se cambio la ubicacion del script del manejo del menu y su referencia

--- Controllers/Ajax/PanelAdministrador/MenuLateral/MenuLateral.js
+++ Controllers/Ajax/PanelAdministrador/MenuLateral/MenuLateral.js
@@ -1,75 +1,21 @@
-<!DOCTYPE html>
-<html>
-   <?php
-session_start();
-$idSesion=isset($_SESSION["IdResponsable"])?$_SESSION["IdResponsable"]:null;
-session_write_close();
-if (!$idSesion)
-        {
-         echo "Debe iniciar sesion para poder continuar";
-         exit;
-        }
-   ?>
-    <head>
-        <title>PanelAdministrador</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" href="../Public/Css/PanelAdministrador/PanelAdministrador.css">
-    </head>
-    <body>
-        
-        <header class="cabezera">
-            <p>Informacion del perfil</p>
-            <img src="../Public/Assets/Administrador_InformacionPerfil_Icono.png" alt="Icono_Administrador">
-        </header>
-        <div class="menuLateral">
-        <div class="botonMenu" botonSeleccionado="true" opcionMenu="botonPerfil">
-        <img src="../Public/Assets/Icono_Administrador_Perfil.png" alt="IconoApartado">
-        </div>
-        <div class="botonMenu" botonSeleccionado="false" opcionMenu="botonClinicas">
-        <img src="../Public/Assets/Icono_Administrador_Clinica.png" alt="IconoApartado">
-        </div>
-        <div class="botonMenu" botonSeleccionado="false" opcionMenu="botonDoctores">
-        <img src="../Public/Assets/Icono_Administraador_Doctor.png" alt="IconoApartado">
-        </div>
-        <div class="botonMenu" botonSeleccionado="false" opcionMenu="botonPacientes">
-        <img src="../Public/Assets/Icono_Administrador_Paciente.png" alt="IconoApartado">
-        </div>
-        <a href="../index.php">
-          <div class="botonMenu" botonSeleccionado="false" >
-        <img src="../Public/Assets/Icono_CerrarSesion.png" alt="IconoApartado" botonSeleccionado="false">
-        </div>
-        </a>
-       
-        </div>
-        <div class="Menu_Dinamico" TipoMenu="Informacion_Administrador">
-        <?php
-        include ("../Models/PanelAdministrador/InfoPerfil.php");
-        include("../config.php");
-        
+document.addEventListener("DOMContentLoaded",function(e)
+{
+const opcionMenuPerfil=document.querySelector('[opcionMenu="botonPerfil"]');
+const opcionMenuClinicas=document.querySelector('[opcionMenu="botonClinicas"]');
+const opcionMenuPacientes=document.querySelector('[opcionMenu="botonPacientes"]');
+const opcionMenuDoctores=document.querySelector('[opcionMenu="botonDoctores"]');
+const divMenuDinamico=document.querySelector('[class="Menu_Dinamico"]');
 
-        
-
-        if (!$idSesion)
-        {
-         echo "Debe iniciar sesion para poder continuar";
-         exit;
-        }
-
-        $objPerfil= new InformacionPerfil();
-        
-        
-        $consultaPerfil=$objPerfil->ObtenerInfoPerfil($conexiondb,$idSesion);
-         $nombre=$consultaPerfil["Nombre"];
-         $apellidoPaterno=$consultaPerfil["ApellidoPaterno"];
-         $apellidoMaterno=$consultaPerfil["ApellidoMaterno"];
-         $numeroTrabajador=$consultaPerfil["NoTrabajador"];
-         $telefono=$consultaPerfil["Telefono"];
-         $contraseña=$consultaPerfil["Contraseña"];
-         $correoElectronico=$consultaPerfil["CorreoElectronico"];
-
-   echo "
-            <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1'>
+opcionMenuPerfil.addEventListener("click",function(e)
+{
+    if (this.getAttribute("botonSeleccionado")=="false")
+    {
+   divMenuDinamico.setAttribute("TipoMenu","Informacion_Administrador");
+   this.setAttribute("botonSeleccionado","true");
+   opcionMenuClinicas.setAttribute("botonSeleccionado","false");
+   opcionMenuPacientes.setAttribute("botonSeleccionado","false");
+   opcionMenuDoctores.setAttribute("botonSeleccionado","false");
+   divMenuDinamico.innerHTML=` <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1'>
                 <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_DivImg'>
                    <img src='../Public/Assets/Icono_Administrador_Perfil.png' alt='IconoAdmin'> 
                 </div>
@@ -149,15 +95,47 @@ if (!$idSesion)
         
        <div class='Menu_Dinamico_AdministrarPerfil_SubApartadoSubmit'>
        <button class='Menu_Dinamico_AdministrarPerfil_SubApartadoSubmit_button' referencia='InfoPerfil_BotonSubmit'>Guardar cambios</button>
-       <div> "
-        ?>
-            
-        </div>
-        
+       <div> `;
+    }
 
-        </div>
-<script src="../Controllers/Ajax/PanelAdministrador/InfoAdmin/PerfilAdministradorRec.js" defer></script>
-<script src="../Controllers/Ajax/PanelAdministrador/MenuLateral/MenuLateral.js" defer></script> 
-    </body>
+});
+opcionMenuClinicas.addEventListener("click",function(e)
+{
+    if (this.getAttribute("botonSeleccionado")=="false")
+        {
+       divMenuDinamico.setAttribute("TipoMenu","Administrar_Clinicas");
+       this.setAttribute("botonSeleccionado","true");
+       opcionMenuPerfil.setAttribute("botonSeleccionado","false");
+       opcionMenuPacientes.setAttribute("botonSeleccionado","false");
+       opcionMenuDoctores.setAttribute("botonSeleccionado","false");
+       divMenuDinamico.innerHTML=`<p>Se modifico</p>`;
+        }
+    
+});
+opcionMenuPacientes.addEventListener("click",function(e)
+{
+    if (this.getAttribute("botonSeleccionado")=="false")
+        {
+       divMenuDinamico.setAttribute("TipoMenu","Administrar_Pacientes");
+       this.setAttribute("botonSeleccionado","true");
+       opcionMenuClinicas.setAttribute("botonSeleccionado","false");
+       opcionMenuPerfil.setAttribute("botonSeleccionado","false");
+       opcionMenuDoctores.setAttribute("botonSeleccionado","false");
+        }
+});
+opcionMenuDoctores.addEventListener("click",function(e)
+{
+    if (this.getAttribute("botonSeleccionado")=="false")
+        {
+       divMenuDinamico.setAttribute("TipoMenu","Administrar_Doctores");
+       this.setAttribute("botonSeleccionado","true");
+       opcionMenuClinicas.setAttribute("botonSeleccionado","false");
+       opcionMenuPacientes.setAttribute("botonSeleccionado","false");
+       opcionMenuPerfil.setAttribute("botonSeleccionado","false");
+        }
+});
 
-</html>
+
+
+
+});

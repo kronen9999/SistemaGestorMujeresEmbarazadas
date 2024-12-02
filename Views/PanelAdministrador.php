4890fe8150c0ea6bab4a19e8a6903f110c7b04bd
kronen9999 <stacica22eneronkio@gmail.com>
2024-12-01 20:34:40 -0600

    Se agrego nueva funcionalidad para el dibujo de html en las paginas principales

--- Views/PanelAdministrador.php
+++ Views/PanelAdministrador.php
@@ -59,97 +59,11 @@ if (!$idSesion)
         $objPerfil= new InformacionPerfil();
         
         
-        $consultaPerfil=$objPerfil->ObtenerInfoPerfil($conexiondb,$idSesion);
-         $nombre=$consultaPerfil["Nombre"];
-         $apellidoPaterno=$consultaPerfil["ApellidoPaterno"];
-         $apellidoMaterno=$consultaPerfil["ApellidoMaterno"];
-         $numeroTrabajador=$consultaPerfil["NoTrabajador"];
-         $telefono=$consultaPerfil["Telefono"];
-         $contraseña=$consultaPerfil["Contraseña"];
-         $correoElectronico=$consultaPerfil["CorreoElectronico"];
+        $htmlObtener=$objPerfil->ObtenerInfoPerfil($conexiondb,$idSesion);
+         
 
-   echo "
-            <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1'>
-                <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_DivImg'>
-                   <img src='../Public/Assets/Icono_Administrador_Perfil.png' alt='IconoAdmin'> 
-                </div>
-                <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1'>
-                    <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput'>
-                      <p>
-                         Nombre:
-                      </p>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div'>
-                      <input type='text'  class='input_Nombre' placeholder='Ingrese su nombre' id='Perfil_Nombre' value='$nombre' InfoPerfilInput='nombre'>  
-                      </div> 
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput'>
-                      <p>
-                         Apellido paterno:
-                      </p>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div'>
-                      <input type='text'  class='input_Nombre' placeholder='Ingrese su apellido paterno' id='Perfil_ApellidoPaterno' value='$apellidoPaterno' InfoPerfilInput='apellidoP'>  
-                      
-                      </div>
-                    </div>
-                    <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput'>
-                      <p>
-                         Apellido Materno:
-                      </p>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div'>
-                      <input type='text'  class='input_Nombre' placeholder='Ingrese su apellido materno' id='Perfil_ApellidoMaterno' value='$apellidoMaterno' InfoPerfilInput='apellidoM'>  
-                      
-                      </div>
-                </div>
-            </div>
-        
-        </div>
-        </div>
-        <div class='Menu_Dinamico_AdministrarPerfil_SubApartado2'>
-            <div class='Menu_Dinamico_AdministrarPerfil_SubApartado2_Div1'>
-            <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput'>
-                      <p>
-                         Numero trabajador:
-                      </p>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div'>
-                      <input type='text' class='input_Nombre' placeholder='Ingrese su numero de trabajador' id='Perfil_Notrabajador' value='$numeroTrabajador' InfoPerfilInput='noTrabajador'>
-
-                      
-                      </div> 
-                      </div>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput'>
-                      <p>
-                         Telefono:
-                      </p>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div'>
-                      <input type='text'  class='input_Nombre' placeholder='Ingrese su Telefono' id='Perfil_Telefono' value='$telefono' InfoPerfilInput='telefono'>  
-                      
-                      </div> 
-                      </div>
-            </div>
-            <div class='Menu_Dinamico_AdministrarPerfil_SubApartado2_Div2'>
-            <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput'>
-                      <p>
-                         Contraseña:(Requiere autenticacion)
-                      </p>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div'>
-                      <input type='password'  class='input_Nombre' placeholder='Ingrese su contraseña' id='Perfil_Contraseña' value='$contraseña'>  
-                      <img src='../Public/Assets/Icono_Editar.png' alt='IconoEditarImputs'>
-                      </div> 
-                      </div>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput'>
-                      <p>
-                         Correo Electronico:
-                      </p>
-                      <div class='Menu_Dinamico_AdministrarPerfil_SubApartado1_Div_1_DivImput_Div'>
-                      <input type='text'  class='input_Nombre' placeholder='Ingrese su Correo electronico' id='Perfil_Correo_Electronico' value='$correoElectronico' InfoPerfilInput='correoE'>  
-                     
-                      </div> 
-                      </div>
-            </div>
-        </div>
-        
-       <div class='Menu_Dinamico_AdministrarPerfil_SubApartadoSubmit'>
-       <button class='Menu_Dinamico_AdministrarPerfil_SubApartadoSubmit_button' referencia='InfoPerfil_BotonSubmit'>Guardar cambios</button>
-       <div> "
+   echo $htmlObtener;
+            
         ?>
             
         </div>

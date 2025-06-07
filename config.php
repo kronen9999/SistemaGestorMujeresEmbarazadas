b05c629d06d1bb0370eb18deec140beea6d76ce4
kronen9999 <stacica22eneronkio@gmail.com>
2025-06-07 07:55:21 -0600

    Se han cambiado las credenciales  para la base de datos en la web

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-$host="localhost";
-$user="root";
-$password="";
-$db="GESTOREMBARAZADAS";
+$host="194.195.84.1";
+$user="u356678976_SGME22";
+$password="@SiGeMuEm22";
+$db="u356678976_SGME";
 
 $conexiondb=mysqli_connect($host,$user,$password,$db);
 
